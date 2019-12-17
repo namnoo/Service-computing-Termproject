dc141e9decfaaeb3398a6e8f0ebed4e64ba2d5ad
jihye <jihye3519@gmail.com>
2019-12-17 18:38:11 +0900

    add api reference page revise module codes revise restapi

--- children_hospital/get_city_code_children.py
+++ children_hospital/get_city_code_children.py
@@ -1,18 +1,17 @@
-#-*- coding:utf-8 -*-
 '''전국 지역 코드를 dictionary 형태의 code.txt로 추출하는 python code'''
+'''추출된 코드를 추가적으로 가공하였음, 따라서 이제 이 코드는 실행 하면 안됨!'''
 from ast import literal_eval
 import requests as re
 import xmltodict as xtd
 import json
-import sys
-import io
 
+# state code dictionary open
 with open('state_code_children_hospital.txt','r',encoding='utf-8') as f:
     state = literal_eval(f.read())
 
 code_dict = {}
 
-#def children_hospital(keywords = None, location = 0):
+# 시/구 별 코드 받아오는 메소드
 def city_code(st_name):
   for i in range(len(state)):
     code_dict[st_name] = {}
@@ -27,9 +26,10 @@ def city_code(st_name):
       rDJ = json.dumps(rD)
       rDD = json.loads(rDJ)
 
-      w_data = rDD["response"]["body"]["items"]["item"] #item에 포함된 data list
-      total_count = int(rDD["response"]["body"]["totalCount"]) #주어진 조건의 total count
+      w_data = rDD["response"]["body"]["items"]["item"]
+      total_count = int(rDD["response"]["body"]["totalCount"])
       temp = []
+
       # total count가 1일 경우 dictionary를 원소로 가지는 list가 생성 되지 않는 예외 처리
       if total_count == 1:
           temp.append(w_data)
@@ -39,11 +39,14 @@ def city_code(st_name):
           code = w_data[e]["cd"]
           name = w_data[e]["cdNm"]
           code_dict[st_name][name] = code
+
+  # 시/구 별 코드를 저장
   with open('city_code_children_hospital.txt', 'w', encoding='utf-8') as f:
       f.write(str(code_dict))
 
-for key in state:
-    city_code(key)
+# 실행 코드
+# for key in state:
+#     city_code(key)
 
 
       
